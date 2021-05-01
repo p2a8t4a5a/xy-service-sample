2da709007763f278ef87f789c7fdef8c2da71964
fresh <1326191992@qq.com>
2021-05-01 10:11:11 +0800

    actuator hystrix

--- src/main/java/com/sc/sample/SampleApplication.java
+++ src/main/java/com/sc/sample/SampleApplication.java
@@ -2,15 +2,20 @@ package com.sc.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.ComponentScan;
 
-@RefreshScope
 @SpringBootApplication
 @EnableDiscoveryClient
 @ComponentScan("com.sc")
+@RefreshScope
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class SampleApplication {
+
     public static void main(String argv[]) {
         SpringApplication.run(SampleApplication.class, argv);
     }
