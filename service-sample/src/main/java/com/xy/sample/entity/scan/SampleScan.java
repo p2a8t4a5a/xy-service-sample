e515935c4479886cc8d8739841e6003dcceec8f0
fresh <1326191992@qq.com>
2021-06-21 12:57:17 +0800

    1

--- service-sample/src/main/java/com/xy/sample/entity/scan/SampleScan.java
+++ service-sample/src/main/java/com/xy/sample/entity/scan/SampleScan.java
@@ -21,7 +21,6 @@ public class SampleScan extends BaseEntity<Long> {
 
     private String name;
     private ScanTypeEnum scanType;
-    private String txId;
     @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone="GMT+8")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime scanTime;
